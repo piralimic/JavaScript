a9a6ab86c614d6d5f46abdbbdd6d9f6b1c077c29
Michael Pirali <pirali.michael@gmail.com>
2019-09-12 16:41:47 +0200

    Update final 09-misc

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -11,5 +11,43 @@
 
 (() => {
   // your code here
-  
+  let textToWave = document.getElementById('target').innerHTML;
+  let lettersToWave = textToWave.split("");
+
+  for (let i = 0; i < lettersToWave.length; i+=6) {
+    lettersToWave[i] = '<span style="position: relative; top: 0px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 1; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: 6px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 2; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: 12px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 3; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: 20px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 4; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: 12px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 5; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: 6px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 7; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: -6px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 8; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: -12px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 9; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: -20px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 10; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: -12px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+  for (let i = 11; i < lettersToWave.length; i+=12) {
+    lettersToWave[i] = '<span style="position: relative; top: -6px; left: '+i*18+'px;">'+lettersToWave[i]+'</span>';
+  }
+
+
+  document.getElementById('target').innerHTML = lettersToWave.join("");
 })();

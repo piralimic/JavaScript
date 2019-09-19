8d655418fa70bd478f3f86004b00de143e840ccb
Michael Pirali <pirali.michael@gmail.com>
2019-09-19 16:59:35 +0200

    Update Hangman-part-3

---
+++ 99-bonus/02-hangman/script.js
@@ -0,0 +1,69 @@
+/* becode/javascript
+ *
+ * /01-base/01-var-alert/script.js - 1.1: var & alert
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+  // your code here
+  const lettresSecretes = [
+    "D",
+    "E",
+    "V",
+    "I",
+    "N",
+    "E",
+    "R"
+  ];
+
+  const lettresDecouvertes = [
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_"
+  ];
+
+  let compteurDeLettres = 0;
+  const listePropositions = [];
+
+  function guessLetter() {
+    let propositionJoueur = prompt("Proposez une lettre : ");
+    const lettresSecretesTotales = lettresSecretes.length;
+
+    if (listePropositions.includes(propositionJoueur)) {
+      alert(`Vous avez déjà proposé la lettre : ${propositionJoueur}`)
+    } else {
+      listePropositions.push(propositionJoueur);
+      for (let i = 0; i < lettresSecretesTotales; i++) {
+        if (lettresSecretes[i] == propositionJoueur) {
+          if (lettresDecouvertes[i] != propositionJoueur) {
+            lettresDecouvertes[i] = propositionJoueur;
+            compteurDeLettres++;
+          }
+        }
+      }
+    }
+
+    document.getElementById("demo").innerText = lettresDecouvertes.join(" ");
+
+
+    if (compteurDeLettres == lettresSecretes.length) {
+      alert('Bien joué! ');
+      console.table(lettresDecouvertes);
+    } else {
+      guessLetter();
+    }
+  }
+
+  document.getElementById("run").addEventListener("click", () => {
+    guessLetter();
+  });
+})();

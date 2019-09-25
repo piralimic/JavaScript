64a81a6589e5d50d68e275681e4502b84ed26cd8
Michael Pirali <pirali.michael@gmail.com>
2019-09-25 12:02:43 +0200

    Update final 11-fetch

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -10,41 +10,61 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
-    const response = await fetch('http://localhost:3000/heroes',{
-      method: 'POST',
-      header: {
-        'Content-Type'
+  // your code here
+  /*
+  newHero({
+      name: "Batman",
+      alterEgo: "Bruce Wayne",
+      abilities: [
+        "Expert in interrogation techniques",
+        "Different fighting styles"
+      ]
+  });
+  */
+  let newHero = {
+    id: "",
+    name: "",
+    alterEgo: "",
+    abilities: []
+  };
+
+  const addInJsonFile = () => {
+    newHero.name = document.querySelector("#hero-name").value;
+    newHero.alterEgo = document.querySelector("#hero-alter-ego").value;
+    newHero.abilities = document.querySelector("#hero-powers").value.split(",");
+  };
+
+  const jsonHeader = new Headers({
+    'Content-Type': 'application/json;charset=utf-8'
+  });
+
+  const checkInputForm = (id) => {
+    if (document.querySelector(id).value == "") {
+      return false;
+    } else
+      return true;
+  };
+
+  document.getElementById('run').addEventListener("click", async () => {
+    if (checkInputForm("#hero-name") && checkInputForm("#hero-alter-ego") && checkInputForm("#hero-powers")) {
+      addInJsonFile();
+
+      let response = await fetch('http://localhost:3000/heroes', {
+        method: 'POST',
+        headers: jsonHeader,
+        body: JSON.stringify(newHero)
+      });
+      if (response.ok) {
+        const responseData = await fetch('http://localhost:3000/heroes');
+        const myJson = await responseData.json();
+        console.log(JSON.stringify(myJson));
+        alert("Le nouveau héro a été enregistré");
+      } else if (response.status == 404) {
+        console.log("L'enregistrement a échoué, la page est actuellement indisponible");
       }
-    });
+    } else {
+      alert("Tous les champs doivent être correctement complétés avant de cliquer sur le bouton couillon.");
+    }
 
-
-
-    document.getElementById('run').addEventListener("click", async () => {
-      const response = await fetch('http://localhost:3000/heroes?id='+document.getElementById("hero-id").value);
-      const listHeroesJson = await response.json();
-      //console.log(JSON.stringify(listHeroesJson));
-      document.getElementById("target").innerHTML = "";
-      // Get the TEMPLATE element:
-      const template = document.getElementById("tpl-hero");
-      // Get the LI element from the template:
-      const item = template.content.querySelector("li");
-      for (let hero of listHeroesJson){
-        item.querySelector("strong").innerText = hero.name;
-        item.querySelector("em").innerText = hero.alterEgo;
-        // afficher les sorts dans une liste
-        const listAbilities = hero.abilities;
-        let p = "<ul>";
-        for (let ability of listAbilities){
-          p += "<li>"+ability+"</li>";
-        }
-        item.querySelector("p").innerHTML = p+"</ul>";
-        //item.querySelector("p").innerText = hero.abilities;
-        // Clone Node and add the new one into the TARGET
-        const clonage = template.content.cloneNode(true);
-        document.getElementById("target").appendChild(clonage);
-      }
-
-
-    });
-  })();
+  });
+})();
--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -10,5 +10,27 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  document.getElementById('run').addEventListener("click", async () => {
+    try {
+      const response = await fetch('http://localhost:3000/heroes/' + document.getElementById("hero-id").value, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        }
+      });
+
+      if (response.ok) {
+        console.log("L'élément a été supprimé");
+      } else if (response.status == 404) {
+        console.log("L'élément n'existe pas");
+      }
+
+    } catch (e) {
+      console.error(e.message);
+    } finally {
+
+    }
+
+  });
 })();
